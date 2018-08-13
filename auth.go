428a3a0cb82a52e78a37aa30fe8b5d5e1650d320
Harits Fahreza Christyonotoputra <harits.fahreza@gmail.com>
2018-08-13 18:26:52 +0700

    Fix GetToken mechanism

--- auth.go
+++ auth.go
@@ -2,6 +2,7 @@ package bca
 
 //AuthToken represents response of BCA OAuth 2.0 response message
 type AuthToken struct {
+	Error
 	AccessToken string `json:"access_token"`
 	TokenType   string `json:"token_type"`
 	ExpiresIn   int    `json:"expires_in"`
--- auth/client.go
+++ auth/client.go
@@ -1,30 +1,44 @@
 package auth
 
 import (
-	"bytes"
 	"context"
 	"encoding/base64"
 	"net/http"
+	"net/url"
+	"strings"
 
 	bca "github.com/haritsfahreza/go-bca"
 )
 
 //Client is used to invoke BCA OAuth 2.0 API
 type Client struct {
-	config bca.Config
-	client bca.ClientImplementation
+	Client       bca.ClientImplementation
+	ClientID     string
+	ClientSecret string
+}
+
+//NewClient is used to initialize new auth.Client
+func NewClient(config bca.Config) Client {
+	return Client{
+		Client:       bca.NewClient(config),
+		ClientID:     config.ClientID,
+		ClientSecret: config.ClientSecret,
+	}
 }
 
 //GetToken is used to get OAuth 2.0 token
 func (c Client) GetToken(ctx context.Context) (bca.AuthToken, error) {
 	path := "/api/oauth/token"
 
-	var header http.Header
-	header.Add("Authorization", base64.StdEncoding.EncodeToString([]byte(c.config.ClientID+":"+c.config.ClientSecret)))
+	header := http.Header{}
+	header.Add("Authorization", "Basic "+base64.StdEncoding.EncodeToString([]byte(c.ClientID+":"+c.ClientSecret)))
+
+	data := url.Values{}
+	data.Add("grant_type", "client_credentials")
 
 	var response bca.AuthToken
-	if err := c.client.CallRaw("POST", path, "application/x-www-form-urlencoded",
-		header, bytes.NewBufferString("grant_type=client_credentials"), response); err != nil {
+	if err := c.Client.CallRaw("POST", path, "application/x-www-form-urlencoded",
+		header, strings.NewReader(data.Encode()), &response); err != nil {
 		return response, err
 	}
 	return response, nil
